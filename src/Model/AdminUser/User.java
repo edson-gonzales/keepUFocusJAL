3940575bca50580726a14f475b55f1c87b0422a4
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2016-07-06 15:31:51 -0400

    First commit of the proyect, Proyect Structure

---
+++ src/Model/AdminUser/User.java
@@ -0,0 +1,12 @@
+package Model.AdminUser;
+
+/**
+ * Created by AldoBalderrama on 7/5/2016.
+ */
+public class User extends Model.Common.Person
+{
+    public User()
+    {
+        super();
+    }
+}
---
+++ src/Model/Common/Person.java
@@ -0,0 +1,8 @@
+package Model.Common;
+
+/**
+ * Created by AldoBalderrama on 7/5/2016.
+ */
+public class Person {
+
+}
