86288906ceb1f2a347370ea6a3a052597eb41b02
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2016-07-14 19:14:46 -0400

    Improved  the class User, Person, UserControllers and add the class ControllerUsersTest, UserTest

--- src/Model/Conection/Connection.java
+++ src/Model/Conection/Connection.java
@@ -7,20 +7,18 @@ import java.sql.SQLException;
 /**
  * Created by Administrator on 7/7/2016.
  */
-public class Connection
-{
-    String url = "C:\\Users\\ALDO\\Documents\\Fundacion-Jala\\PROG-102\\keepUFocusJAL\\dataBase\\dbFocusUTime.sqlite";
+public class Connection {
+    String url = "\\\\172.20.0.16\\mt01at01\\AT01\\Database\\dbFocusUTime.sqlite";
     public java.sql.Connection connectDataBase;
 
-    public void connect()
-    {
+    public void connect() {
         try {
             connectDataBase = DriverManager.getConnection("jdbc:sqlite:" + url);
-            if (connectDataBase!=null) {
+            if (connectDataBase != null) {
                 System.out.println("Connect ok");
             }
-        }catch (SQLException ex) {
-            System.err.println("Error connect \n"+ex.getMessage());
+        } catch (SQLException ex) {
+            System.err.println("Error connect \n" + ex.getMessage());
         }
     }
 
@@ -32,16 +30,16 @@ public class Connection
             e.printStackTrace();
         }
     }
-    public boolean execute(String sql)
-    {
+
+    public boolean execute(String sql) {
         try {
             connect();
             PreparedStatement ps = connectDataBase.prepareStatement(sql);
             ps.execute();
             ps.close();
             return true;
-        }catch(SQLException e){
-            System.err.println( e.getMessage() );
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
         }
         return false;
     }
