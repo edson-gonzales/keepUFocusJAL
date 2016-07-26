fc5312fdcaa047753cf9141de4fb926bdb7e0705
JuanaRodriguez <Juana.Rodriguez@fundacion-jala.org>
2016-07-26 18:53:25 -0400

    code review for Role since of the observations

--- TestModel/RoleTest.java
+++ TestModel/RoleTest.java
@@ -9,26 +9,29 @@ public class RoleTest {
     @Test
     public void VerifyThatANewRoleWasAdd() {
         Role role = new Role();
-        role.setRoleName("Administrator2");
+        role.setRoleName("Administrator");
         role.save();
-        Role recoverRole = new Role();
-        recoverRole.getRoleById(role.getRoleId());
-        Assert.assertEquals("Administrator2", recoverRole.getRoleName());
-        //Assert.assertNotNull(role.getRoleById(role.getRoleId()));
-
+        Assert.assertEquals(role.getRoleName(), Role.getRoleById(role.getRoleId()).getRoleName());
     }
 
     @Test
     public void VerifyThatARoleWasUpdated() {
         Role role = new Role();
-        role.setRoleName("Gerent Edited1");
-        role.update(1);
-        Assert.assertEquals("Gerent Edited1", role.getRoleName());
+        role.setRoleName("s");
+        role.save();
+        Role otherRole = Role.getRoleById(role.getRoleId());
+        otherRole.setRoleName("Administrator");
+        otherRole.update();
+        Assert.assertEquals(otherRole.getRoleName(), Role.getRoleById(role.getRoleId()).getRoleName());
+        role.delete();
     }
 
     @Test
     public void VerifyThatARoleWasDeleted() {
         Role role = new Role();
-        role.delete(2);
+        role.setRoleName("Administrator");
+        role.save();
+        role.delete();
+        Assert.assertNull(Role.getRoleById(role.getRoleId()));
     }
 }
--- src/Model/Common/Role.java
+++ src/Model/Common/Role.java
@@ -59,10 +59,10 @@ public class Role {
     }
 
     /**
-     * This method insert a role in the database, catch the id from the role inserted in the "result" variable and
-     * have the "saved" flag that help us to check if the role was inserted
+     * This method insert a role in the database, catch the id from the role inserted in the "result" variable
      *
-     * @return saved
+     * @return the saved flag that help us to check if the role was inserted, this  flag return true if the
+     * role was inserted correctly.
      */
     public boolean save() {
         boolean saved = false;
@@ -76,8 +76,6 @@ public class Role {
             if (result.next()) {
                 this.setRoleId(result.getInt(1));
                 saved = true;
-            } else {
-                throw new SQLException("There is an error, It was not possible to insert the Role");
             }
             result.close();
             dbAccess.closeConnection();
@@ -93,9 +91,10 @@ public class Role {
      * @param roleId
      * @return the object role
      */
-    public Role getRoleById(int roleId) {
+    public static Role getRoleById(int roleId) {
         Role role = null;
         ResultSet result = null;
+        DataAccess dbAccess = new DataAccess();
         try {
             StringBuilder sql = new StringBuilder("Select * from Role ");
             sql.append(String.format("where roleId = %s;", roleId));
@@ -117,14 +116,13 @@ public class Role {
      * This method updates a role and give back true through of updated variable if the date was modify successfully
      * using the method update from DataAccess class.
      *
-     * @param roleId
-     * @return updated
+     * @return updated true if the date was modify successfully and false if it wasn't modify
      */
-    public boolean update(int roleId) {
+    public boolean update() {
         boolean updated = false;
         StringBuilder sql = new StringBuilder("update Role set ");
         sql.append(String.format("roleName = '%s' ", this.getRoleName()));
-        sql.append(String.format("where roleId = %s", roleId));
+        sql.append(String.format("where roleId = %s", getRoleId()));
         updated = dbAccess.update(sql.toString());
         dbAccess.closeConnection();
         return updated;
@@ -132,15 +130,14 @@ public class Role {
 
     /**
      * This method deletes a role and give back true through of deleted variable if the date was delete successfully
-     * using the method deleted from DataAccess class.
+     * using the method deleted from DataAccess class.     *
      *
-     * @param roleId
-     * @return deleted
+     * @return deleted true if the date was deleted successfully
      */
-    public boolean delete(int roleId) {
+    public boolean delete() {
         boolean deleted = false;
         StringBuilder sql = new StringBuilder("delete from Role ");
-        sql.append(String.format("where roleId = %s", roleId));
+        sql.append(String.format("where roleId = %s", getRoleId()));
         deleted = dbAccess.delete(sql.toString());
         dbAccess.closeConnection();
         return deleted;
