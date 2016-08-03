8a21244967825f573c1a8e3550f4d79def3a1b20
JuanaRodriguez <Juana.Rodriguez@fundacion-jala.org>
2016-08-03 12:50:46 -0400

    Code review for  Applications, ApplicationHandler and its unit test

--- src/Model/Applications/Application.java
+++ src/Model/Applications/Application.java
@@ -5,87 +5,135 @@ import Model.Connections.DataAccess;
 import java.sql.ResultSet;
 
 /**
- * Created by AldoBalderrama on 7/25/2016.
+ * The class manages application of the database
+ *
+ * @autor: JuanaRodriguez
  */
 public class Application {
     private int applicationId;
     private String applicationName;
-    private int applicationCategoryId;
+    private int categoryId;
+    private DataAccess dbAccess;
 
+    /**
+     * The method return the Id of the application
+     *
+     * @return applicationId, this variable is an integer value
+     */
     public int getApplicationId() {
         return applicationId;
     }
 
+    /**
+     * The method set the id of the application
+     *
+     * @param applicationId will be set in the application
+     */
     public void setApplicationId(int applicationId) {
         this.applicationId = applicationId;
     }
 
+    /**
+     * The method return the name of the application
+     *
+     * @return applicationName, this variable is an string value
+     */
     public String getApplicationName() {
         return applicationName;
     }
 
+    /**
+     * The method set the name of the application
+     *
+     * @param applicationName will be set in the application
+     */
     public void setApplicationName(String applicationName) {
         this.applicationName = applicationName;
     }
 
-    public int getApplicationCategoryId() {
-        return applicationCategoryId;
+    /**
+     * The method return the categoryId of the application
+     *
+     * @return categoryId, this variable is an integer value
+     */
+    public int getCategoryId() {
+        return categoryId;
     }
 
-    public void setApplicationCategoryId(int applicationCategoryId) {
-        this.applicationCategoryId = applicationCategoryId;
+    /**
+     * The method set the categoryId of the application
+     *
+     * @param categoryId will be set in the application
+     */
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
     }
 
+    /**
+     * The method build an Application object
+     */
     public Application() {
-        this.applicationId = 0;
-        this.applicationName = "";
-        this.applicationCategoryId = 0;
+        this.setApplicationId(0);
+        this.setApplicationName("");
+        this.setCategoryId(1);
+        dbAccess = new DataAccess();
     }
 
-    public Application(String applicationName) {
-        this.applicationId = 0;
-        this.applicationName = applicationName;
-        this.applicationCategoryId = 0;
-    }
-
+    /**
+     * The method insert a application in the database, catch the id from the application inserted in the "result"
+     * variable
+     *
+     * @return the saved flag that help us to check if the application was inserted, this  flag return true if the
+     * application was inserted correctly.
+     */
     public boolean save() {
-        System.out.printf("method will implements");
-        return true;
-    }
-
-    public boolean update() {
-        System.out.printf("method will implements");
-        return true;
-    }
-
-    public boolean delete() {
-        System.out.printf("method will implements");
-        return true;
-    }
-
-    public static Application getApplication(int applicationId) {
-        Application application = new Application("Ruby Mine");
-        return application;
-    }
-
-    public static Application getApplicationById(int applicationId) {
-        Application application = null;
+        boolean saved = false;
         ResultSet result = null;
-        DataAccess dbAccess = new DataAccess();
         try {
-            StringBuilder sql = new StringBuilder("Select * from Application ");
-            sql.append(String.format("where applicationId = %s;", applicationId));
-            result = dbAccess.getDataById(sql.toString());
+            StringBuilder sql = new StringBuilder();
+            sql.append("insert into Application (applicationName,applicationCategoryId) ");
+            sql.append(String.format("Values('%s',1)", this.getApplicationName()));
+            result = dbAccess.save(sql.toString());
             if (result.next()) {
-                application = new Application();
-                application.setApplicationId(result.getInt(1));
-                application.setApplicationName(result.getString(2));
+                this.setApplicationId(result.getInt(1));
+                saved = true;
             }
             result.close();
             dbAccess.closeConnection();
         } catch (Exception e) {
-            System.err.println("SQLException: " + e.getMessage());
+            System.out.println("SQLException: " + e.getMessage());
         }
-        return application;
+        return saved;
+    }
+
+    /**
+     * The method updates an application and give back true through of updated variable if the date was modify
+     * successfully using the method update from DataAccess class.
+     *
+     * @return updated true if the date was modify successfully and false if it wasn't modify
+     */
+    public boolean update() {
+        boolean updated = false;
+        StringBuilder sql = new StringBuilder("update Application set ");
+        sql.append(String.format("applicationName = '%s' ", getApplicationName()));
+        sql.append(String.format("where applicationId = %s", getApplicationId()));
+        updated = dbAccess.update(sql.toString());
+        dbAccess.closeConnection();
+        return updated;
+    }
+
+    /**
+     * The method deletes an application and give back true through of deleted variable if the date was delete
+     * successfully using the method deleted from DataAccess class.
+     *
+     * @return deleted true if the date was deleted successfully
+     */
+    public boolean delete() {
+        boolean deleted = false;
+        StringBuilder sql = new StringBuilder("delete from Application ");
+        sql.append(String.format("where applicationId = %s", getApplicationId()));
+        deleted = dbAccess.delete(sql.toString());
+        dbAccess.closeConnection();
+        return deleted;
     }
 }
--- src/Test/Model/Application/ApplicationTest.java
+++ src/Test/Model/Application/ApplicationTest.java
@@ -1,8 +1,54 @@
 package Test.Model.Application;
 
+import Model.Applications.Application;
+import Model.Applications.ApplicationHandler;
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
- * Created by Administrator on 7/28/2016.
+ * @autor: JuanaRodriguez
  */
 public class ApplicationTest {
+    @Test
+    public void VerifyThatANewApplicationIsAdd() {
+        Application application = new Application();
+        application.setApplicationName("Chrome");
+        application.setCategoryId(1);
+        application.save();
+        Assert.assertEquals(application.getApplicationName(), ApplicationHandler.getApplicationById(application.getApplicationId()).getApplicationName());
+        application.delete();
+    }
 
+    @Test
+    public void VerifyThatAnApplicationIsUpdated() {
+        Application application = new Application();
+        application.setApplicationName("Crome");
+        application.setCategoryId(1);
+        application.save();
+        Application otherApplication = ApplicationHandler.getApplicationById(application.getApplicationId());
+        otherApplication.setApplicationName("Chrome");
+        otherApplication.update();
+        Assert.assertEquals(otherApplication.getApplicationName(), ApplicationHandler.getApplicationById(application.getApplicationId()).getApplicationName());
+        application.delete();
+    }
+
+    @Test
+    public void VerifyIfAnApplicationIsDeleted() {
+        Application application = new Application();
+        application.setApplicationName("Chrome");
+        application.setCategoryId(1);
+        application.save();
+        application.delete();
+        Assert.assertNull(ApplicationHandler.getApplicationById(application.getApplicationId()));
+    }
+
+    @Test
+    public void VerifyIfAnApplicationExist() {
+        Application application = new Application();
+        application.setApplicationName("Excel");
+        application.setCategoryId(1);
+        application.save();
+        Assert.assertTrue(ApplicationHandler.exist("Excel"));
+        application.delete();
+    }
 }
